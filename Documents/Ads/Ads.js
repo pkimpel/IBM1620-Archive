700905281fd5c6b337e1399eceb575f7069eaefc
Dave Babcock <dave@babcock-family.org>
2025-10-15 21:57:38 -0700

    Added top Documents page and Documents/Ads page.

--- Documents/Ads/Ads.js
+++ Documents/Ads/Ads.js
@@ -1,19 +1,24 @@
 /***********************************************************************
-* IBM1620.org/Documents/Documents.js
+* IBM1620.org/Documents/Ads/Ads.js
 ************************************************************************
 * Copyright (c) 2025, Paul Kimpel & Dave Babcock.
 * Licensed under the MIT License, see
 *       http://www.opensource.org/licenses/mit-license.php
 ************************************************************************
-* IBM 1620 Archive Documents page routines.
+* IBM 1620 Archive Documents Ads page routines.
 ************************************************************************
 * 2025-10-15  D.Babcock
 *   Initial version.
 * For later revisions, see the IBM1620-Archive github repository.
 ***********************************************************************/
 
-import {Section} from "../Common/Section.js";
+import {Section} from "../../Common/Section.js";
+import {TableSort} from "../../Common/TableSort.js";
 
 window.addEventListener("load", function() {
-    Section.buildMenu(Section.documentsPosition, 1);
+    Section.buildMenu(Section.documentsPosition, 2);
 }, {once: true});
+
+for (const item of document.querySelectorAll("#AdsTable THEAD A")) {
+    item.addEventListener("click", TableSort.sortAColumn);
+}
